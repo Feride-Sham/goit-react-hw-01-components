4562e161042014e82e9c8dba0257e7852ee6c04d
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-02-24 18:37:13 +0300

    init: add husky

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <h1>Hello</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
 
-export default App;
+//   );
+// }
+
+// export default App;
